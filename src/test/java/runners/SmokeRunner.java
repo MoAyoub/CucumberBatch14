fa7c206ca0fe98f062f117ff42dc16735b3cd0e7
MoAyoub <mohdayoub50@gmail.com>
2022-12-07 10:58:12 -0600

    first commit

---
+++ src/test/java/runners/SmokeRunner.java
@@ -0,0 +1,35 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        //features we use to provide the path of all the feature files
+        features = "src/test/resources/features/",
+        glue = "steps",
+        dryRun= false,
+        //when you set dry run to true, it stops actual execution
+        //it will quickly scan all the gherkin steps whether they are
+        //implemented or not
+
+//if you wanna run specific sprint
+        tags="@sprint1",
+
+//to remove irrelevant information from console, you need to set monochrome to true
+        monochrome = true,
+    //pretty keyword prints the steps in the console to increase readability
+        plugin = {"pretty"}
+
+
+
+
+
+)
+
+
+
+public class SmokeRunner {
+}
